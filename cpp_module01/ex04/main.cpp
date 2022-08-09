823df5e161232fa742669a461c5e325449a3a9f8
Achraf Maghous <acmaghou@e3r11p13.1337.ma>
2022-08-09 18:17:05 +0100

    handled ex04 errors

--- cpp_module01/ex04/main.cpp
+++ cpp_module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 15:24:29 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/09 17:57:08 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/08/09 18:15:53 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ std::string	replaceStrings(std::string file, std::string s1, std::string s2)
 int	main(int ac, char **av)
 {
 	std::ifstream	file;
-	std::string		infileName;
-	std::ofstream	output;
+	std::string		infile;
 	std::string		tmp;
 	std::string		fileCon;
 	std::string		out;
@@ -39,9 +38,9 @@ int	main(int ac, char **av)
 	std::string		s2;
 	if (ac == 4)
 	{
-		infileName = av[1];
+		infile = av[1];
 		file.open(av[1]);
-		if (file.is_open())
+		if (file.is_open() && file.good())
 		{
 			while(getline(file, tmp))
 			{
@@ -49,11 +48,25 @@ int	main(int ac, char **av)
 				fileCon.append("\n");
 			}
 		}
+		else {
+			std::cerr << "Cannot open: " << infile << std::endl;
+			return (1);
+		}
 		s1.assign(av[2]);
 		s2.assign(av[3]);
 		out = replaceStrings(fileCon, s1, s2);
-		infileName.append(".replace");
-		output.open(infileName);
-		output << out;
+		std::ofstream outfile(infile + ".replace");
+		if (!outfile)
+		{
+			std::cerr << "Cannot open or create: " << infile + ".replace" << std::endl;
+			return (2);
+		}
+		outfile << out;
 	}
+	else
+	{
+		std::cerr << "number of arguments required is 4 including executable.\n";
+		return (3);
+	}
+	return (0);
 }

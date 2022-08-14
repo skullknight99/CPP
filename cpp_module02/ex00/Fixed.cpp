e55a0e148c620737237036c063d39639b12ccaf8
Achraf Maghous <acmaghou@e2r12p5.1337.ma>
2022-08-14 10:21:44 +0100

    hehe

--- cpp_module02/ex00/Fixed.cpp
+++ cpp_module02/ex00/Fixed.cpp
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/09 09:53:44 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/14 10:21:17 by acmaghou         ###   ########.fr       */
+/*   Created: 2022/08/14 10:06:55 by acmaghou          #+#    #+#             */
+/*   Updated: 2022/08/14 10:06:56 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanB.hpp"
-
-HumanB::HumanB(std::string name) {
-	this->name = name;
-}
-
-HumanB::~HumanB() {
-	
-}
-
-void	HumanB::setWeapon(Weapon &wp)
-{
-	this->wp = &wp;
-}
-
-void	HumanB::Attack() {
-	if (wp)
-		std::cout << this->name << " attacks with their " << wp->getType() << std::endl;
-}
--- cpp_module02/ex00/Fixed.hpp
+++ cpp_module02/ex00/Fixed.hpp
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/09 09:53:44 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/14 10:21:17 by acmaghou         ###   ########.fr       */
+/*   Created: 2022/08/14 10:06:58 by acmaghou          #+#    #+#             */
+/*   Updated: 2022/08/14 10:15:03 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanB.hpp"
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
-HumanB::HumanB(std::string name) {
-	this->name = name;
-}
-
-HumanB::~HumanB() {
-	
-}
-
-void	HumanB::setWeapon(Weapon &wp)
+class Fixed
 {
-	this->wp = &wp;
-}
+	private:
 
-void	HumanB::Attack() {
-	if (wp)
-		std::cout << this->name << " attacks with their " << wp->getType() << std::endl;
-}
+	public:
+		Fixed();
+		~Fixed();
+};
+
+
+#endif

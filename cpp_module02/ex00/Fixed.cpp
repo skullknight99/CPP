65e1a6f79cc6f7b6b364ef28299f0a1e96278ea2
Achraf Maghous <acmaghou@e2r12p5.1337.ma>
2022-08-15 13:00:10 +0100

    ex00 cpp02 done

--- cpp_module02/ex00/Fixed.cpp
+++ cpp_module02/ex00/Fixed.cpp
@@ -6,28 +6,32 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 10:06:55 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/15 12:44:29 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/08/15 12:54:14 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+//---------------Constructor--------------
 Fixed::Fixed()
 {
 	this->store = 0;
 	std::cout << "Default constructor called" << std::endl;
 }
 
+//---------------Destructor---------------
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
+//-------------Copy Constructor-----------
 Fixed::Fixed(Fixed &obj) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = obj;
 }
 
+//----------Copy Assignment Overload------
 Fixed&	Fixed::operator= (Fixed& obj)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
@@ -35,11 +39,13 @@ Fixed&	Fixed::operator= (Fixed& obj)
 	return *this;
 }
 
+//-----------Getter Function--------------
 int	Fixed::getRawBits( void ) const {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->store);
 }
 
+//-----------Setter Function--------------
 void	Fixed::setRawBits(int const raw) {
 	this->store = raw;
 }

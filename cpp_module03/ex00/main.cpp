5c93a826b0dd311099668552689ad64034bc98e1
Achraf Maghous <acmaghou@e3r11p13.1337.ma>
2022-08-25 14:19:45 +0100

    ex00 done

--- cpp_module03/ex00/main.cpp
+++ cpp_module03/ex00/main.cpp
@@ -6,30 +6,40 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 13:36:24 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/25 12:00:11 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:19:30 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int	main( void ) {
-	ClapTrap	x("Iori");
-	while (x.getmana() || x.gethp())
+	ClapTrap	x("Aatrox");
+	ClapTrap	y("Ornn");
+	while (y.gethp() || x.gethp())
 	{
+		std::cout << x.getname() << std::endl;
 		std::cout << "Hit Points: " << x.gethp() << std::endl;
 		std::cout << "Mana: "<< x.getmana() << std::endl;
 		std::cout << "Attack Damage: "<< x.getad() << std::endl;
 		std::cout << "============================" << std::endl;
-		x.attack("Kyo Kusanagi");
-		x.takeDamage(0);
-		x.beRepaired(1);
+		std::cout << y.getname() << std::endl;
+		std::cout << "Hit Points: " << y.gethp() << std::endl;
+		std::cout << "Mana: "<< y.getmana() << std::endl;
+		std::cout << "Attack Damage: "<< y.getad() << std::endl;
+		std::cout << "============================" << std::endl;
+		x.attack(y.getname());
+		y.takeDamage(1);
+		y.attack(x.getname());
+		y.beRepaired(0);
+		x.beRepaired(3);
+		std::cout << ".//////////////////." << std::endl;
 		std::cout << "summary of the round:" << std::endl;
-		if (x.gethp() <= 0)
+		if (y.gethp() <= 0 || x.gethp() <= 0)
 		{
-			std::cout << x.getname() << " is dead" << std::endl;
+			std::cout << y.getname() << " is dead" << std::endl;
 			break;
 		}
-		if (x.getmana() <= 0)
+		if (x.getmana() == 0 || y.getmana() == 0)
 		{
 			std::cout << "not enough mana to cast an attack or a heal" << std::endl;
 			break;
--- cpp_module03/ex01/ScavTrap.hpp
+++ cpp_module03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 16:31:34 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/08/25 09:54:30 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/08/25 12:49:04 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,7 @@
 
 class ScavTrap : public ClapTrap
 {
-	protected:
-		std::string name;
-		int			hp;
-		int			mana;
-		int			ad;
-	public:
-		ScavTrap(std::string name);
-		~ScavTrap();
-		ScavTrap(const ScavTrap &st);
-		ScavTrap&	operator= (const ScavTrap &st);
+	
 };
 
 

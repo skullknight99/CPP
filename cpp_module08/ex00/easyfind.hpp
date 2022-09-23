7c8d9f7ef50468e5c31c3d7261ec439a8cd69c31
Achraf Maghous <acmaghou@e2r12p5.1337.ma>
2022-09-23 14:55:44 +0100

    cpp08 ex00 done

--- cpp_module08/ex00/easyfind.hpp
+++ cpp_module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 12:38:15 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/09/23 13:02:52 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:23:28 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <algorithm>
 #include <stdexcept>
 #include <list>
+#include <array>
 #include <deque>
 
 template <typename T>   typename T::iterator easyfind(T &arr ,int n)
--- cpp_module08/ex00/main.cpp
+++ cpp_module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 12:38:18 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/09/23 13:03:18 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:55:40 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,15 @@ int main()
     {
         std::cerr << e.what() << '\n';
     }
+    // try
+    // {
+    //     std::array <int, 4> x = {12, -55, 777, -666};
+    //     int dist = std::distance(x.begin(), easyfind(x, 777));
+    //     std::cout << "iterator is: " << dist << std::endl;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
     
 }

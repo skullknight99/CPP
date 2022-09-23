c7dbd400347e33f0fa7f3cb9465051b0a7b76863
Achraf Maghous <acmaghou@e2r12p5.1337.ma>
2022-09-23 13:02:53 +0100

    cpp08 ex00 done

--- cpp_module08/ex00/easyfind.hpp
+++ cpp_module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 12:38:15 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/09/23 13:01:20 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/09/23 13:02:52 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@
 
 template <typename T>   typename T::iterator easyfind(T &arr ,int n)
 {
-    typename T::iterator t;
-    t = std::find(arr.begin(), arr.end(), n);
-    if (t == arr.end())
+    typename T::iterator it;
+    it = std::find(arr.begin(), arr.end(), n);
+    if (it == arr.end())
        throw std::invalid_argument("could not find number");
-    return (t);
+    return (it);
 }
 
 #endif

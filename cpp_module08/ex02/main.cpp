a0b25317bc88e622282c19bb2524e836e5a57a06
Achraf Maghous <acmaghou@e2r12p5.1337.ma>
2022-09-25 15:35:48 +0100

    cpp08 ex02 done

--- cpp_module08/ex02/main.cpp
+++ cpp_module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acmaghou <acmaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 11:34:36 by acmaghou          #+#    #+#             */
-/*   Updated: 2022/09/25 14:44:16 by acmaghou         ###   ########.fr       */
+/*   Updated: 2022/09/25 15:34:06 by acmaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	main() {
 	std::cout << "top of the stack: "<< mstack.top() << std::endl;
 
 	mstack.pop();
-	
-	std::cout << "size of the stack: "<< mstack.size() << std::endl;
+
+	std::cout << "size of the MutantStack: "<< mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(9);
@@ -30,6 +30,8 @@ int	main() {
 	mstack.push(777);
 	mstack.push(0);
 
+	std::cout << "size of the MutantStack: "<< mstack.size() << std::endl;
+	
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
@@ -40,36 +42,6 @@ int	main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "size of inherited stack s from mstack: " << s.size() << std::endl;
 	return (0);
 }
-
-// int	main() {
-// 	std::list<int>	mstack;
-
-// 	mstack.push_back(5);
-// 	mstack.push_back(17);
-	
-// 	std::cout << "top of the stack: "<< mstack.front() << std::endl;
-
-// 	mstack.pop_front();
-	
-// 	std::cout << "size of the stack: "<< mstack.size() << std::endl;
-
-// 	mstack.push_back(3);
-// 	mstack.push_back(9);
-// 	mstack.push_back(737);
-// 	mstack.push_back(777);
-// 	mstack.push_back(0);
-
-// 	std::list<int>::iterator it = mstack.begin();
-// 	std::list<int>::iterator ite = mstack.end();
-
-// 	++it;
-// 	--it;
-// 	while (it != ite) {
-// 		std::cout << *it << std::endl;
-// 		++it;
-// 	}
-// 	std::stack<int> s(mstack);
-// 	return (0);
-// }
